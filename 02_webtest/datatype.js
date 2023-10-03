09798797039187017bcb1a78f7af06b2f90b6a40
teamofindia <40353780+teamofindia@users.noreply.github.com>
2023-10-03 12:19:39 +0000

    data types

---
+++ 02_webtest/datatype.js
@@ -0,0 +1,5 @@
+"use  strict" //treat all js  code as newer version
+let age =120;
+console.log(typeof( "avinash"));
+console.log(typeof null);
+ console.log(typeof undefined);
---
+++ 02_webtest/usingfuction.js
@@ -0,0 +1,8 @@
+let score= 33;
+let isboolean =Boolean(score);
+//console.log(isboolean);
+ let mark= 192;
+  let CovertString =String(mark);
+  //console.log(CovertString);
+  //console.log(typeof CovertString);
+  
