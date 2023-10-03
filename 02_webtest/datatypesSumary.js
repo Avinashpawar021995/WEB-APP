7b5f4c9c360cf94a65d85438d16875bc6c688dd2
teamofindia <40353780+teamofindia@users.noreply.github.com>
2023-10-03 15:11:46 +0000

    data type  sumary

---
+++ 02_webtest/datatypesSumary.js
@@ -0,0 +1,10 @@
+// primitive
+// 7 types : String , number , boolean , null, undefined,Symbol, BigInt.
+
+//references type (non primitive)
+// Array, Objects,, Functions.
+ // java Script in dymanic types
+
+ const id=Symbol('123')
+ const anotherId=Symbol('123')
+ console.log(id===anotherId);
